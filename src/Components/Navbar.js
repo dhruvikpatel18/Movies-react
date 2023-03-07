282b3a185980319de09f05425ed1290dcf09488f
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-03-07 16:45:52 +0530

    Navbar issue fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,11 +4,10 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <div style={{display:'flex',padding:'0.5'}}>
+            <div style={{display:'flex',padding:'', background:'lightgray'}}>
                 <Link to="/" style={{textDecoration:'none'}}><h1 style={{marginTop:'1rem',marginLeft:'1rem'}}>Movies App</h1></Link>
-                <Link to="/favourites" style={{textDecoration:'none'}}><h2 style={{marginLeft:'2rem',marginTop:'1.5rem'}}>Favourites</h2></Link>
-                
-                
+                <Link to="/favourites" style={{textDecoration:'none'}}><h2 style={{marginLeft:'65rem',marginTop:'1.5rem'}}>Favourites</h2></Link>
+            
             </div>
         )
     }
